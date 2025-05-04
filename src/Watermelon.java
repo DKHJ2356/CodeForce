bd127d033e9336b6a8cb4ee464a88ebf729f472a
DKHJ <domingokafil2003@gmail.com>
2025-05-04 20:55:46 +0600

    Forgot how to upload specific file

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Watermelon {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int w = sc.nextInt();
+
+        if( w>= 1 && w<= 100 ){
+            if(w%2 == 0){
+                if( w/2 == 1) System.out.println("No");
+                else System.out.println("YES");
+            }
+            else System.out.println("NO");
+        }
     }
 }
