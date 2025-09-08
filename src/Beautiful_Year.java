752766b70425a33c1627867d8e8f2f421b871430
DKHJ2356 <domingokafil2003@gmail.com>
2025-09-08 08:24:19 +0600

    Finally solved Beautiful year problem. Lucky number was easy but did not understand the question properly. So take it slow when solving problems

--- src/Beautiful_Year.java
+++ src/Beautiful_Year.java
@@ -4,34 +4,38 @@ public class Beautiful_Year {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int count = 0;
+        int y = sc.nextInt();
         int [] arr = new int[4];
+        int number = 0;
 
-        while(n/10!=0){
-            arr[count] = n%10;
-            count++;
-            n/=10;
-        }
+            for (int i = y+1; i < 10000;i++) {
 
-        if(n/10==0){
-            arr[count] = n%10;
-        }
+                int flag = 0;
+                int x = i;
+                int j = 0;
 
-        int flag = 0;
+                while (x != 0) {
+                    arr[j++] = x % 10;
+                    x /= 10;
+                }
 
-        for(int i = 0 ; i<count-1; i++){
-            for(int j = i+1; j<count; j++){
-                if(arr[i]==arr[j]){
-                    flag = 1;
+                for (int k = 0; k < arr.length; k++) {
+                    for (int l = k + 1; l < arr.length; l++) {
+                        if (arr[k] == arr[l]) {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if(flag == 1) break;
+                }
+
+                if(flag == 0) {
+                    number = i;
                     break;
                 }
+
             }
-        }
 
-        if(flag == 0) System.out.println("Ok");
-        else System.out.println("Not ok");
-
-
+        System.out.println(number);
     }
 }
